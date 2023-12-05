6742b2b6e789a7b70801c17608c7e714244ffb76
Mateus Rosa <mateusrosa0901@gmail.com>
2023-12-05 11:04:19 -0300

    directions

--- resources/views/pedidos/create.blade.php
+++ resources/views/pedidos/create.blade.php
@@ -9,6 +9,14 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@100;200;300;400;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/assets/css/users/create.css">
     <link rel="stylesheet" href="/assets/css/pedidos/create.css">
+    <script>
+        (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+        key: "AIzaSyA-SfDxtbKlXS6AgOPpQZ4epZnf-zjMeYs",
+        v: "weekly",
+        // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+        // Add other bootstrap parameters as needed, using camel case.
+        });
+  </script>
 @endsection
 
 @section('content')
@@ -63,15 +71,7 @@
                     <input class="sub-buttom" type="submit" value="Nova Entrega">
                 </form>
 
-                <iframe
-                    width="500"
-                    height="450"
-                    style="border:2"
-                    loading="lazy"
-                    allowfullscreen
-                    referrerpolicy="no-referrer-when-downgrade"
-                    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA-SfDxtbKlXS6AgOPpQZ4epZnf-zjMeYs&q={{urlencode(Auth::user()->logradouro.','. Auth::user()->numero.','. Auth::user()->bairro.','. Auth::user()->cidade.','. Auth::user()->uf)}}">
-                </iframe>
+                <div id="map"></div>
             </div>
         </div>
     </div>
@@ -105,6 +105,32 @@
                 }
             })
         })
+    </script>
+
+
+    <script>
+        let map;
+        function initMap() {
+            const { Map } = google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+            const { directionsService } = google.maps.importLibrary("DirectionsService") as google.maps.DirectionsService;
+            const { directionsRenderer } = google.maps.importLibrary("DirectionsRenderer") as google.maps.DirectionsRenderer;
+            const position = { lat: -15.344, lng: 200.031 };
+
+            map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 4,
+                center: position,
+                mapId: "terrain", 
+            });
+
+            directionsRenderer.setMap(map);
+
+            directionsService.route({
+                origin: 'Toronto, Canadá',
+                destination: 'Montreal, Canadá',
+                travelMode: google.maps.TravelMode.DRIVING,
+            });
+        }
+        initMap();
     </script>
 
 @endsection
